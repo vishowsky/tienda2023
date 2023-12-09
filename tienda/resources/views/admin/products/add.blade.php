e1365fcfca59cf0c8041e68b56bd5685eda9fa47
Vicente Carreño <vishowsky@gmail.com>
2023-12-09 18:13:44 -0300

    add category

--- tienda/resources/views/admin/products/add.blade.php
+++ tienda/resources/views/admin/products/add.blade.php
@@ -17,56 +17,64 @@
     <div class="container-fluid">
         <div class="panel shadow">
             <div class="header">
-                <h2 class="title">
-                   Agregar Productos
-                </h2>
-                <div class="inside">
-                    {!! Form::open(['url' => '/admin/product/add']) !!}
-                    <div class="row">
-
-                        <div class="col-md-6">
-                            <label for="name">Nombre del producto</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon">@</span>
-                                </div>
-                                {!! Form::text('name', null,['class' => 'form-control'])!!}
-                            </div>
-                        </div>
-                            <div class="col-md-3">
-                                <label for="name">Categoria</label>
-                            </div>
-
-                            <div class="col-md-3">
-                                <label for="name">Imagen Destacada</label>
-                                <div class="custom-file">
-                                {!! Form::file('img',['class' => 'form-control','id' => 'customFile']) !!}
-                                <label class="custom-file-label" for="customFile">Elige el archivo</label>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        {!! Form::close()!!}
-                        <div class="row mt16">
-                            <div class="col-md-3">
-                                <label for="price">Precio</label>
-                                {!! Form::number('price',null, ['class' => 'form-control' , 'min' =>'0' ]) !!}
-                            </div>
-                        </div>
-    
-                        <div class="row mt16">
-                            <div class="col-md-12">
-                                <label for="content" >Descripcion</label>
-                                {!!Form::textarea('content', null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-                        {!! Form::close() !!}
-                    </div>
-                    </div>
-                    
+                <h2 class="title"> Agregar Productos</h2>
             </div>
-        </div>
+            <div class="inside">
+                {!! Form::open(['url' => '/admin/product/add'])!!}
+                <div class="row">
+                    <div class="col-md-6">
+                    <label for="name" class="">Nombre del producto</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">
+                                <i class="far fa-keyboard"></i>
+                            </span>
+                        </div>
+                        {!! Form::text('name', null,['class' => 'form-control']) !!}
+                    </div>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="name">Categoria:</label>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="name">Imagen Destacada</label>
+                        <div class="custom-file">
+                        {!! Form::file('img',['class'=> 'custom-file-input', 'id' => 'customFile'])!!}
+                        <label class="custom-file-label" for="customFile" >Agregar imagen</label>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="row mt16"> 
+                <div class="col-md-3">
+                    <label for="price">Precio</label>
+                    <div class="input-group">
+                        {!! Form::number('Price',null,['class' => 'form-control', 'min' => '0', 'step' => 'any']) !!}
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <label for="price">En oferta?</label>
+                    <div class="input-group">
+                        {!! Form::select('indiscount', ['0' => 'No', '1' => 'Si'],0, ['class' => 'custom-select'])!!}
+                    </div>
+                </div>
+
+            </div>
+
+                <div class="row mt16">
+                    <div class="col-md-12">
+                        <label for="content">Descripcion</label>
+                        {!! Form::textarea('content',null, ['class'=>'form-control'] )!!}
+                    </div>
+                </div>
+                {!!Form::close() !!}
+            </div>
+        </div>         
     </div>
+</div>
+
 
 
 @endsection
